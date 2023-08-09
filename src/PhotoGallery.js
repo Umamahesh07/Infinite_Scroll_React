cc677f25976551765328d3c7265faa9023ec4edb
Uma Mahesh <amanchi.umamaheshwar@gmail.com>
2023-08-09 19:49:18 -0400

    Implementation of Infinite Scroll using Time API

--- src/PhotoGallery.js
+++ src/PhotoGallery.js
@@ -3,31 +3,29 @@ import React, { useState, useEffect } from 'react';
 function PhotoGallery() {
     const [photos, setPhotos] = useState([]);
     const [page, setPage] = useState(1);
-    const [isLoading, setIsLoading] = useState(false); // New loading state
+    const [isLoading, setIsLoading] = useState(false);
 
     const API_URL = `https://api.unsplash.com/photos?client_id=1_C5M2HSNJlHtadBKSHTUVwgukkcTdEFGv20_YNF8Fk&page=`;
-    
+
     useEffect(() => {
-        // If it's already loading, we don't make another request.
         if (isLoading) return;
 
-        setIsLoading(true); // Set loading to true when starting to fetch
+        setIsLoading(true);
 
         fetch(API_URL + page)
             .then(res => res.json())
             .then(data => {
                 setPhotos(prevPhotos => [...prevPhotos, ...data]);
-                setIsLoading(false);  // Reset loading state after data is set
+                setIsLoading(false);
             })
             .catch(error => {
                 console.error("Error fetching photos:", error);
-                setIsLoading(false); // Reset loading state if an error occurs
+                setIsLoading(false);
             });
     }, [page]);
 
     useEffect(() => {
         const handleScroll = () => {
-            // Adjusted threshold for loading more content
             if (window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight - 50) {
                 setPage(prevPage => prevPage + 1);
             }
